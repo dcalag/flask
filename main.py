d3210c7c5278d63cda083655b046eea2ac80e8ab
dchavez10 <daniel_chavez_10@hotmail.com>
2016-08-26 04:02:22 -0500

    d

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+
 #from flask import Flask, request, jsonify
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash
 from db import *
 import jsonpickle
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/')
 def login():
@@ -16,10 +22,10 @@ def processLogin():
     user = User()
     user.username = request.form['username']
     user.password = request.form['password']
-    user.username += "Q"
-    #return render_template('login.html', model=user)
+    flash("Usuario y contraseña correctos (" + user.username + ")." )
+    return render_template('login.html', model=user)
     #return json.dumps({par1:'OK','user':user,'pass':password});
-    return jsonpickle.encode(user)
+    #return jsonpickle.encode(user)
 
 if __name__ == '__main__':
   app.run()
